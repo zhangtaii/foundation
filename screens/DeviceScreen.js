39c6183aa0007fa50adafe41da03e3b476b8ec72
Tai Zhang <q6112345@gmail.com>
2018-06-24 07:00:32 +0800

    Update DeviceScreen

--- screens/DeviceScreen.js
+++ screens/DeviceScreen.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React from "react";
 import {
   Image,
   Platform,
@@ -6,55 +6,116 @@ import {
   StyleSheet,
   Text,
   TouchableOpacity,
+  TouchableHighlight,
   View,
-  Button,
-} from 'react-native';
-import { WebBrowser } from 'expo';
+  Modal,
+  Button
+} from "react-native";
+import { WebBrowser } from "expo";
 
-import { MonoText } from '../components/StyledText';
-import { ListItem } from 'react-native-elements'
+import { MonoText } from "../components/StyledText";
+import { ListItem, Slider } from "react-native-elements";
+
+import { FontAwesome } from "@expo/vector-icons";
 
 const list = [
   {
-    title: 'Ceilling Lamp',
-    icon: 'lightbulb-o',
+    title: "Ceilling Lamp",
+    icon: "lightbulb-o",
     switched: true,
+    deviceID: 1
   },
   {
-    title: 'Decorative Light',
-    icon: 'lightbulb-o',
+    title: "Decorative Light",
+    icon: "lightbulb-o",
     switched: false,
-  },
-]
+    deviceID: 2
+  }
+];
 
 export default class DeviceScreen extends React.Component {
   static navigationOptions = {
-    title: 'Device',
-
+    title: "Device"
   };
+
+  state = {
+    isGroupExpanded: true,
+    modalVisible: false
+  };
+
+  setModalVisible(visible) {
+    this.setState({ modalVisible: visible });
+  }
 
   render() {
     return (
       <View style={styles.container}>
-        <ScrollView style={styles.container} contentContainerStyle={styles.contentContainer}>
-          {
+        <ScrollView
+          style={styles.container}
+          contentContainerStyle={styles.contentContainer}
+        >
+          <TouchableOpacity
+            onPress={() => {
+              this.setState({ isGroupExpanded: !this.state.isGroupExpanded });
+            }}
+            activeOpacity={0.6}
+            style={{
+              flexDirection: "row",
+              alignItems: "center",
+              paddingHorizontal: 5
+            }}
+          >
+            <Text style={{ fontSize: 15, marginRight: 5 }}>Bedroom</Text>
+            {this.state.isGroupExpanded ? (
+              <FontAwesome name="angle-up" size={18} />
+            ) : (
+              <FontAwesome name="angle-down" size={18} />
+            )}
+            />
+          </TouchableOpacity>
+
+          {this.state.isGroupExpanded &&
             list.map((item, i) => (
               <ListItem
+                onPress={(item)=>{
+                  this.setModalVisible(true);
+                }}
                 key={i}
                 title={item.title}
-                leftIcon={{ name: item.icon,  type:'font-awesome' }}
+                leftIcon={{ name: item.icon, type: "font-awesome" }}
                 switchButton
                 hideChevron
                 switched={item.switched}
-                onSwitch={(status)=>{
+                onSwitch={status => {
                   list[i].switched = status;
                   this.forceUpdate();
+                  const level = status ? 255 : 0;
+                  fetch(
+                    `https://agent.electricimp.com/Sz15XCQ9JCxM?vals=${
+                      item.deviceID
+                    },${level}`
+                  );
                 }}
               />
-            ))
-          }
-
+            ))}
         </ScrollView>
+        <Modal
+          animationType="slide"
+          transparent={true}
+          visible={this.state.modalVisible}
+          onRequestClose={() => {
+            //alert('Modal has been closed.');
+          }}
+        >
+          <View style={{ marginTop: 22 }}>
+            <View>
+              <Slider
+                value={this.state.value}
+                onValueChange={value => this.setState({ value })}
+              />
+            </View>
+          </View>
+        </Modal>
       </View>
     );
   }
@@ -69,8 +130,8 @@ export default class DeviceScreen extends React.Component {
 
       return (
         <Text style={styles.developmentModeText}>
-          Development mode is enabled, your app will be slower but you can use useful development
-          tools. {learnMoreButton}
+          Development mode is enabled, your app will be slower but you can use
+          useful development tools. {learnMoreButton}
         </Text>
       );
     } else {
@@ -83,12 +144,14 @@ export default class DeviceScreen extends React.Component {
   }
 
   _handleLearnMorePress = () => {
-    WebBrowser.openBrowserAsync('https://docs.expo.io/versions/latest/guides/development-mode');
+    WebBrowser.openBrowserAsync(
+      "https://docs.expo.io/versions/latest/guides/development-mode"
+    );
   };
 
   _handleHelpPress = () => {
     WebBrowser.openBrowserAsync(
-      'https://docs.expo.io/versions/latest/guides/up-and-running.html#can-t-see-your-changes'
+      "https://docs.expo.io/versions/latest/guides/up-and-running.html#can-t-see-your-changes"
     );
   };
 }
@@ -96,88 +159,88 @@ export default class DeviceScreen extends React.Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: "#fff"
   },
   developmentModeText: {
     marginBottom: 20,
-    color: 'rgba(0,0,0,0.4)',
+    color: "rgba(0,0,0,0.4)",
     fontSize: 14,
     lineHeight: 19,
-    textAlign: 'center',
+    textAlign: "center"
   },
   contentContainer: {
-    paddingTop: 30,
+    paddingTop: 10
   },
   welcomeContainer: {
-    alignItems: 'center',
+    alignItems: "center",
     marginTop: 10,
-    marginBottom: 20,
+    marginBottom: 20
   },
   welcomeImage: {
     width: 100,
     height: 80,
-    resizeMode: 'contain',
+    resizeMode: "contain",
     marginTop: 3,
-    marginLeft: -10,
+    marginLeft: -10
   },
   getStartedContainer: {
-    alignItems: 'center',
-    marginHorizontal: 50,
+    alignItems: "center",
+    marginHorizontal: 50
   },
   homeScreenFilename: {
-    marginVertical: 7,
+    marginVertical: 7
   },
   codeHighlightText: {
-    color: 'rgba(96,100,109, 0.8)',
+    color: "rgba(96,100,109, 0.8)"
   },
   codeHighlightContainer: {
-    backgroundColor: 'rgba(0,0,0,0.05)',
+    backgroundColor: "rgba(0,0,0,0.05)",
     borderRadius: 3,
-    paddingHorizontal: 4,
+    paddingHorizontal: 4
   },
   getStartedText: {
     fontSize: 17,
-    color: 'rgba(96,100,109, 1)',
+    color: "rgba(96,100,109, 1)",
     lineHeight: 24,
-    textAlign: 'center',
+    textAlign: "center"
   },
   tabBarInfoContainer: {
-    position: 'absolute',
+    position: "absolute",
     bottom: 0,
     left: 0,
     right: 0,
     ...Platform.select({
       ios: {
-        shadowColor: 'black',
+        shadowColor: "black",
         shadowOffset: { height: -3 },
         shadowOpacity: 0.1,
-        shadowRadius: 3,
+        shadowRadius: 3
       },
       android: {
-        elevation: 20,
-      },
+        elevation: 20
+      }
     }),
-    alignItems: 'center',
-    backgroundColor: '#fbfbfb',
-    paddingVertical: 20,
+    alignItems: "center",
+    backgroundColor: "#fbfbfb",
+    paddingVertical: 20
   },
   tabBarInfoText: {
     fontSize: 17,
-    color: 'rgba(96,100,109, 1)',
-    textAlign: 'center',
+    color: "rgba(96,100,109, 1)",
+    textAlign: "center"
   },
   navigationFilename: {
-    marginTop: 5,
+    marginTop: 5
   },
   helpContainer: {
     marginTop: 15,
-    alignItems: 'center',
+    alignItems: "center"
   },
   helpLink: {
-    paddingVertical: 15,
+    paddingVertical: 15
   },
   helpLinkText: {
     fontSize: 14,
-    color: '#2e78b7',
-  },
+    color: "#2e78b7"
+  }
 });
